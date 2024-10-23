3cfb54b6424bbf7855e54deb7ce7bbe4322a80b7
ploshxh <149713941+ploshxh@users.noreply.github.com>
2024-10-23 12:14:57 -0500

    Add files via upload

---
+++ Practica1.java
@@ -0,0 +1,27 @@
+package practica1;
+
+
+public class Practica1 {
+
+ 
+    public static void main(String[] args) {
+        
+        int [] age = (12, 45, 67, 6, 24);
+        
+        for (int i=0,i <5;i++ )
+        {
+            
+            System.out.println("la edad en" + "La posicion" + i + "es" + age [i]);
+        }
+        
+    int y=0;
+    
+    while (y<5)
+    { 
+    
+         System.out.println("la edad en" + "La posicion" + y + "es" + age [y]);
+         
+         y++;
+    }
+    
+}
